b971877d1ed8bf0a92a59fad8a1366993c7eebf6
rikonnect <chachaerickjo@gmail.com>
2022-04-16 13:37:32 +0300

    ft - add form for collecting username and inject the component to the root component.

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { FetchService } from 'src/app/services/search.service';
 
 @Component({
   selector: 'app-search',
@@ -7,7 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fetchSerive:FetchService) { }
+
+  onSubmit(term:NgForm){
+
+  }
 
   ngOnInit(): void {
   }
