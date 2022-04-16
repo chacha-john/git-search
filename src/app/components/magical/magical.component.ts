67f1bf015401ea556797317c84234853f36645dc
rikonnect <chachaerickjo@gmail.com>
2022-04-16 10:51:22 +0300

    ft- add navabar component

---
+++ src/app/components/magical/magical.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-magical',
+  templateUrl: './magical.component.html',
+  styleUrls: ['./magical.component.css']
+})
+export class MagicalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
