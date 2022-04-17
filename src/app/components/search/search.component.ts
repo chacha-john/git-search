46a9de8b6977c913c5c68ef0fb3dcc62f89e2706
rikonnect <chachaerickjo@gmail.com>
2022-04-17 05:01:22 +0300

    ft - implement class user and service to get information for user searched

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { User } from 'src/app/classes/user';
 import { FetchService } from 'src/app/services/search.service';
 
 @Component({
@@ -9,14 +10,21 @@ import { FetchService } from 'src/app/services/search.service';
 })
 export class SearchComponent implements OnInit {
 
-  userInfo: any
+  userInfo: User = new User('','','','','','','','')
 
   constructor(private fetchSerive:FetchService) { }
 
   onSubmit(term:NgForm){
     this.fetchSerive.getInfo(term.value.username).subscribe((res)=>{
-      this.userInfo = res
-      console.log(this.userInfo);
+      this.userInfo.bio = res.bio
+      this.userInfo.name = res.name
+      this.userInfo.repos = res.public_repos
+      this.userInfo.followers = res.followers
+      this.userInfo.following = res.following
+      this.userInfo.gists = res.public_gists
+      this.userInfo.avatar = res.avatar_url
+      this.userInfo.login = res.login
+      
     })
 
     
